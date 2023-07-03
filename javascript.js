ac4c779e7930cfc00417a9b27d8692411e88d4a4
JeremyVoldrich <36611861+JeremyVoldrich@users.noreply.github.com>
2023-07-03 17:45:18 -0400

    Create function for game play
    Set up players choice and computers choice.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
-// Set up computers play
+// Set up function computers play
+
 function getComputerChoice() {
   let compChoice = Math.floor(Math.random() * 3);
   if (compChoice === 0) {
@@ -11,3 +12,32 @@ function getComputerChoice() {
 }
 console.log(getComputerChoice());
 
+// Set up function for round of play for players choice and computers choice
+
+function playRound(playerChoice, computerChoice) {
+	if (playerChoice.toLowerCase() === "rock" && computerChoice === "rock") {
+  	return "Tied!";
+  } else if (playerChoice.toLowerCase() === "rock" && computerChoice === "paper") {
+  	return "You lose! Paper beats rock.";
+  } else if (playerChoice.toLowerCase() === "rock" && computerChoice === "scissors") {
+    return "You win! Rock beats scissors.";
+	} else if (playerChoice.toLowerCase() === "paper" && computerChoice === "rock") {
+  	return "You win! Paper beats rock.";
+  } else if (playerChoice.toLowerCase() === "paper" && computerChoice === "paper") {
+  	return "Tied!";
+  } else if (playerChoice.toLowerCase() === "paper" && computerChoice === "scissors") {
+  	return "You lose! Scissors beats paper.";
+  } else if (playerChoice.toLowerCase() === "scissors" && computerChoice === "rock") {
+  	return "You lose! Rock beats scissors.";
+  } else if (playerChoice.toLowerCase() === "scissors" && computerChoice === "paper") {
+  	return "You win! Scissors beats paper.";
+  } else if (playerChoice.toLowerCase() === "scissors" && computerChoice === "scissors") {
+  	return "Tied!";
+  } else {
+  	return "Invalid entry!"
+  }
+}
+
+const playerChoice = "rock";
+const computerChoice = "scissors";
+console.log(playRound(playerChoice, computerChoice));
